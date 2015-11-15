605a9d45bf4a0731571c0d9d38879c6493ed1c7d
Yonezawa Izumi <iyonezawa@mate-en.com>
2015-11-15 15:48:12 +0900

    CB version

--- src/main/scala/scaloid/example/HelloScaloid.scala
+++ src/main/scala/scaloid/example/HelloScaloid.scala
@@ -5,18 +5,23 @@ import android.graphics.Color
 
 import scala.util.{Success, Failure}
 
-class HelloScaloid extends SActivity with TagUtil {
+class HelloScaloid extends SActivity with TagUtil with Logger with WebSocketStateListener {
   self =>
 
   implicit val exec = ExecutionContext.exec
 
   lazy val meToo = new STextView("Me too")
   lazy val webSocket = {
-    new WebSocketPF(WebSocketParameters(
+    /*new WebSocketPF(WebSocketParameters(
       "ws://echo.websocket.org",
       5000,
       ""
-    ))
+    ))*/
+    new WebSocketCB(WebSocketParameters(
+      "ws://echo.websocket.org",
+      5000,
+      ""
+    ), scala.ref.WeakReference(this))
   }
 
   private var counter = 1
@@ -42,7 +47,7 @@ class HelloScaloid extends SActivity with TagUtil {
 
   def onClick(): Unit = {
     info(s"onClick $counter")
-    info(s"socket open: ${webSocket.isOpen}")
+    /*info(s"socket open: ${webSocket.isOpen}")
     info(s"socket ready: ${webSocket.isReady}")
     if (counter == 10) {
       webSocket.close()
@@ -51,6 +56,32 @@ class HelloScaloid extends SActivity with TagUtil {
       future.onSuccess { case s => info(s) }
       future.onFailure { case t => error(t.getMessage) }
     }
+    counter += 1*/
+    if (counter == 10) {
+      webSocket.close()
+
+      val queue = new ShutdownableBlockingDeque[WebSocketData]
+      queue.offer(WebSocketDataFinalize)
+      queue.offer(WebSocketDataFinalize)
+      queue.offer(WebSocketDataFinalize)
+      queue.offer(WebSocketDataFinalize)
+      queue.shutdown().get.foreach(d => self.info(s"test: ${d.toString}"))
+    } else {
+      val data = new WebSocketData(s"aaa${counter}bbb") {
+        val activity = new scala.ref.WeakReference(self)
+        override def onSuccess(response: String): Unit = {
+          activity.get.foreach(a => runOnUiThread(a.info(s"socket result $request => $response")))
+        }
+        override def onFailure(e: Throwable): Unit = {
+          activity.get.foreach(a => runOnUiThread(a.error(s"socket failure $request => ${e.toString}")))
+        }
+      }
+      webSocket.request(data)
+    }
     counter += 1
+  }
+
+  def onSocketResult(e: Throwable): Unit = {
+    runOnUiThread(error(s"onSocketResult ${e.toString}"))
   }
 }
--- src/main/scala/scaloid/example/WebSocket.scala
+++ src/main/scala/scaloid/example/WebSocket.scala
@@ -3,8 +3,12 @@ package scaloid.example
 import com.neovisionaries.ws.client._
 import org.scaloid.common._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.{Promise, Future}
-import scala.util.Try
+import scala.reflect.ClassTag
+import scala.util.{Success, Failure, Try}
+
+import scala.collection.JavaConversions._
 
 case class WebSocketParameters(uri: String, timeout: Int, cookie: String)
 
@@ -94,5 +98,132 @@ class WebSocketPF(parameters: WebSocketParameters)
   override def onError(s: WebSocket, cause: WebSocketException): Unit = {
     error(cause.getMessage)
   }
+
+}
+
+class WebSocketData(val request: String) {
+  def onSuccess(response: String): Unit = {}
+  def onFailure(error: Throwable): Unit = {}
+}
+
+case object WebSocketDataFinalize extends WebSocketData("")
+
+trait WebSocketStateListener {
+  def onSocketResult(result: Throwable): Unit
+}
+
+class ShutdownableBlockingDeque[T <: AnyRef](implicit c:ClassTag[T])
+  extends java.util.concurrent.LinkedBlockingDeque[T] {
+
+  private var enabled: Boolean = true
+
+  def shutdown(): Option[Iterator[T]] = synchronized {
+    if (enabled) {
+      enabled = false
+      val items = toArray(Array[T]())
+      clear()
+      Some(items.iterator)
+    } else {
+      None
+    }
+  }
+
+  override def offer(d: T): Boolean = synchronized {
+    if (enabled) super.offer(d)
+    else false
+  }
+
+  override def offerFirst(d: T): Boolean = synchronized {
+    if (enabled) super.offerFirst(d)
+    else false
+  }
+}
+
+case object AlreadyClosedException extends RuntimeException
+case object ClosingException extends RuntimeException
+case class SendException(frame: String) extends RuntimeException
+
+class WebSocketCB(parameters: WebSocketParameters, listener: scala.ref.WeakReference[WebSocketStateListener])
+  extends WebSocketAdapter
+  with Runnable
+  with TagUtil {
+
+  private val queue = new ShutdownableBlockingDeque[WebSocketData]
+  private val waitings = new scala.collection.mutable.HashMap[Int, WebSocketData]
+
+  new Thread(this).start()
+
+  def request(data: WebSocketData): Boolean = {
+    if (queue.offer(data)) {
+      true
+    } else {
+      data.onFailure(AlreadyClosedException)
+      false
+    }
+  }
+
+  def close(): Boolean = queue.offerFirst(WebSocketDataFinalize)
+
+  def run(): Unit = {
+    val factory = new WebSocketFactory
+    try {
+      val socket = factory.createSocket(parameters.uri, parameters.timeout)
+        .addHeader("cookie", parameters.cookie)
+        .addListener(this)
+        .connect()
+      Iterator.continually(queue.take()).forall({
+        case WebSocketDataFinalize =>
+          queue.shutdown().get.foreach(_.onFailure(ClosingException))
+          waitings.synchronized {
+            val list = waitings.toList
+            waitings.clear()
+            list
+          }.foreach(_._2.onFailure(ClosingException))
+          socket.disconnect()
+          false
+        case data: WebSocketData =>
+          waitings.synchronized(waitings += ((data.hashCode, data)))
+          socket.sendText(s"${data.hashCode} ${data.request}")
+          true
+      })
+    } catch {
+      case e: WebSocketException => listener.get.foreach(_.onSocketResult(e))
+    }
+  }
+
+  protected def parseFrame(message: String): Option[(WebSocketData, String)] = {
+    try {
+      val responses = message.split(" ", 2)
+      if (responses.length < 2) {
+        None
+      } else {
+        val hashCode = java.lang.Integer.parseInt(responses(0))
+        waitings.get(hashCode) match {
+          case Some(data) =>
+            waitings.synchronized(waitings -= hashCode)
+            Some(data, responses{1})
+          case None =>
+            None
+        }
+      }
+    } catch {
+      case e: Throwable => None
+    }
+  }
+
+  override def onTextMessage(s: WebSocket, message: String): Unit = {
+    parseFrame(message) match {
+      case Some((data, response)) =>
+        data.onSuccess(response)
+      case None =>
+        error(s"unknown message $message")
+    }
+  }
+
+  override def onDisconnected(s: WebSocket, sf: WebSocketFrame, cf: WebSocketFrame, closedByServer: Boolean): Unit =
+    if (closedByServer) listener.get.foreach(_.onSocketResult(ClosingException))
+
+  override def onSendError(s: WebSocket, cause: WebSocketException, frame: WebSocketFrame): Unit =
+    listener.get.foreach(_.onSocketResult(SendException(if (frame != null) frame.getPayloadText else "")))
 
 }
